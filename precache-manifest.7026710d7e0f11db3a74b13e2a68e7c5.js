bcbe75b551562f9968a042e6a8df90f77ff16e49
Yeng Vang <yenghuevang@hotmail.com>
2020-06-15 17:33:49 -0500

    Updates

---
+++ precache-manifest.7026710d7e0f11db3a74b13e2a68e7c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ba98b65d182f435381123cea79a9c2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e9d9c318d1b1af28d35f",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/react-portfolio/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d9c318d1b1af28d35f",
+    "url": "/react-portfolio/static/js/main.83df06a5.chunk.js"
+  },
+  {
+    "revision": "e37dd59c01196ab9b4d8",
+    "url": "/react-portfolio/static/js/runtime-main.c1c03dd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
