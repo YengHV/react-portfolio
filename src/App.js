bfc6d110ff8cec6b647cc4aa375c7d9ffe097a78
Yeng Vang <yenghuevang@hotmail.com>
2020-06-29 23:02:50 -0500

    All comp pages working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import RepoComp from './components/RepoComp/Repositories'
 import AboutMeComp from './components/AboutMeComp/AboutMe'
+import Contact from './components/ContactComp/Contact'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       </h1>
       <RepoComp></RepoComp>
       <AboutMeComp></AboutMeComp>
+      <Contact></Contact>
 
     </div>
   );
---
+++ src/components/ContactComp/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+function ContactComp() {
+    return (
+        <div>
+            <h1>Contact Comp Page Working! Not a page yet, will become one</h1>
+            
+        </div>
+    )
+}
+
+export default ContactComp ;
