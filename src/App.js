82cb918a9155769d6f24712e910b25f3f89c3437
Yeng Vang <yenghuevang@hotmail.com>
2020-07-01 20:20:59 -0500

    React Router working for Bootstrap NavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import RepoComp from './components/RepoComp/Repositories'
 import AboutMeComp from './components/AboutMeComp/AboutMe'
-import Contact from './components/ContactComp/Contact'
+import ContactComp from './components/ContactComp/Contact'
+import {Navbar, Nav, Form, FormControl, Button, NavDropdown} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    <Router>
     <div className="App">
-      <h1>
-        Start up app
-      </h1>
-      <Route path = '/' component = {AboutMeComp}/>
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="/">Yeng Vang</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/Contact">Contact</Nav.Link>
+      <Nav.Link href="/Repo">Repositories</Nav.Link>
+
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+      <Switch>
+      <Route path = '/Contact' component = {ContactComp}/>
       <Route path = '/Repo' component = {RepoComp}/>
+      <Route path = '/' component = {AboutMeComp}/>
+      </Switch>
     </div>
     </Router>
+    
   );
 }
 
