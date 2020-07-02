ffc6f39b359f56a0011b2c63eab213a0934b8c4c
Yeng Vang <yenghuevang@hotmail.com>
2020-07-01 20:31:30 -0500

    started about me comp

--- src/components/AboutMeComp/AboutMe.js
+++ src/components/AboutMeComp/AboutMe.js
@@ -2,7 +2,10 @@ import React from "react";
 function AboutMeComp() {
     return (
         <div>
-            <h1>AboutMe Comp Page Working! Not a page yet, will become one</h1>
+            <h1>AboutMe</h1>
+            <h2>Image here</h2>
+            <h4>How I started Web Development</h4>
+            <p>Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words </p>
             
         </div>
     )
