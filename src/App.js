2e3a2bdc96c97ee9038b9f76314dd6beb93d1a5e
Yeng Vang <yenghuevang@hotmail.com>
2020-07-01 19:47:58 -0500

    React router started, but not complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router} from 'react-router-dom'
 import RepoComp from './components/RepoComp/Repositories'
 import AboutMeComp from './components/AboutMeComp/AboutMe'
 import Contact from './components/ContactComp/Contact'
 
 function App() {
   return (
+   <Router>
     <div className="App">
       <h1>
         Start up app
       </h1>
-      <RepoComp></RepoComp>
-      <AboutMeComp></AboutMeComp>
-      <Contact></Contact>
-
+      <Route path = '/' component = {AboutMeComp}/>
+      <Route path = '/Repo' component = {RepoComp}/>
     </div>
+    </Router>
   );
 }
 
