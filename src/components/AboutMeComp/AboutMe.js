434493b2bdc0c9f1bcbbdaa2ec470b8b1943dfcf
Yeng Vang <yenghuevang@hotmail.com>
2020-07-09 18:39:53 -0500

    working on background color

--- src/components/AboutMeComp/AboutMe.js
+++ src/components/AboutMeComp/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMeComp() {
             <h1>AboutMe</h1>
             <h2>Image here</h2>
             <h4>How I started Web Development</h4>
-            <p>Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words Words words words words </p>
+            <p>In my free time, I'd like to go fishing, hunting, and camping. I was born in Saint Paul, Minnsota, and lived there my whole life. My recent postition was a Sales Consultant at AutoNation Ford White Bear lake. I've been working as a Sales Consultant for over 4 months, and in sales for almost my whole work career. In the end of Feburary, I decided to make a career change. This lead me to take a persue The University of Minnesota Continuing Education to learn Full-Stack Web Development. </p>
             
         </div>
     )
