2596b7205ee361d2f35871a83519a4fab4f87900
Yeng Vang <yenghuevang@hotmail.com>
2020-07-13 20:54:19 -0500

    started contact me page

--- src/components/ContactComp/Contact.js
+++ src/components/ContactComp/Contact.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 function ContactComp() {
     return (
         <div>
-            <h1>Contact Comp Page Working! Not a page yet, will become one</h1>
-            
+            <Container>
+            <h1>Contact me Here at:</h1>
+            <h4>yenghuevang@gmail.com</h4>
+            <h4>https://github.com/YengHV</h4>
+            <h4>https://www.linkedin.com/in/yeng-vang-b510a71a3/</h4>
+            </Container>
         </div>
     )
 }
