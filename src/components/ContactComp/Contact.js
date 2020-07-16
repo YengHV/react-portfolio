ba25760fa02f605da129c2f330dc3deab39c3b3d
Yeng Vang <yenghuevang@hotmail.com>
2020-07-15 23:15:40 -0500

    icons working and styled

--- src/components/ContactComp/Contact.js
+++ src/components/ContactComp/Contact.js
@@ -7,10 +7,6 @@ function ContactComp() {
     return (
         <div>
             <Container>
-            <h1>Contact me Here at:</h1>
-            <h4>yenghuevang@gmail.com</h4>
-            <h4>https://github.com/YengHV</h4>
-            <h4>https://www.linkedin.com/in/yeng-vang-b510a71a3/</h4>
             <div>
                 <SocialFollow></SocialFollow>
             </div>
