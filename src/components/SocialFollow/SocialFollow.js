0aa3766b238883fd2a86fe0092ae186c185c2483
Yeng Vang <yenghuevang@hotmail.com>
2020-07-16 20:58:12 -0500

    update social follow

--- src/components/SocialFollow/SocialFollow.js
+++ src/components/SocialFollow/SocialFollow.js
@@ -10,7 +10,7 @@ import {
 function SocialFollow (){
     return (
         <div className ="social-container">
-            <h3>Email me @yenghuevang@gmail.com</h3>
+            <h3>My Email yenghuevang@gmail.com</h3>
             <a href="https://github.com/YengHV" className="github social"><FontAwesomeIcon icon={faGithub} size="2x"></FontAwesomeIcon></a>
             <a href="https://www.linkedin.com/in/yeng-vang-b510a71a3/" className="linkedin social"><FontAwesomeIcon icon={faLinkedin} size="2x"></FontAwesomeIcon></a>
             <a href="https://www.linkedin.com/in/yeng-vang-b510a71a3/" className="twitter social"><FontAwesomeIcon icon={faTwitter} size="2x"></FontAwesomeIcon></a>
