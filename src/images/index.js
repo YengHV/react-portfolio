9d92a76e398b62a005869d817bb959ec8afeb04f
Yeng Vang <yenghuevang@hotmail.com>
2020-07-22 16:34:42 -0500

    moved and update export routes

--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,4 @@
-export const parkplannerimg = require("./parkplannerimg.png");
-export const parkplannerimg2 = require("./parkplannerimg2.png");
-export const parkplannerimg3 = require("./parkplannerimg3.png");
+export const parkplannerimg = require("./parkplannerimgs/parkplannerimg.png");
+export const parkplannerimg2 = require("./parkplannerimgs/parkplannerimg2.png");
+export const parkplannerimg3 = require("./parkplannerimgs/parkplannerimg3.png");
 
